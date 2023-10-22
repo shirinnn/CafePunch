08444339002ba40c4f4c33e895f0445b028ccc89
shirinnn <shirinoh88@gmail.com>
2023-10-23 07:31:57 +0800

    Initialize Springboot

---
+++ cafe_punch/src/main/java/com/seveneleven/cafe_punch/CafePunchApplication.java
@@ -0,0 +1,13 @@
+package com.seveneleven.cafe_punch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafePunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafePunchApplication.class, args);
+	}
+
+}
---
+++ cafe_punch/src/test/java/com/seveneleven/cafe_punch/CafePunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seveneleven.cafe_punch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafePunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
